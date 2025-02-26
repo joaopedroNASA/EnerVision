4816e07e34cb4135b1c6aaf9ce5c197d33b2d330
octavio <octavioferreira.sesisenai@gmail.com>
2025-02-26 08:18:18 -0300

    Nome do grafico

--- view/GraficoView.php
+++ view/GraficoView.php
@@ -4,7 +4,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="estilo.css">
+    <link rel="stylesheet" href="style2.css">
     <title>Gráfico de Consumo</title>
     <script type="text/javascript" src="https://www.gstatic.com/charts/loader.js"></script>
     <script type="text/javascript">
